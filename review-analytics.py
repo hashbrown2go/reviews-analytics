6002cf976252c0d337e53c99225cbc1bd8967047
hashbrown2go <cskplh0705@hotmail.com>
2018-09-27 20:35:05 +0800

    final

--- review-analytics.py
+++ review-analytics.py
@@ -4,5 +4,13 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 10000 == 0:
 			print(len(data))
+i = 0
+for words in line:
+	nums = len(words)
+	nums = int(nums)
+	print(i)
+	i += nums
+
+print((i)/len(data))
